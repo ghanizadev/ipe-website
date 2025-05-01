64a2f67ada1fe602a17e7e3aac320c4854e198af
ghanizadev <jf.melo6@gmail.com>
2025-05-01 22:04:51 +0300

    Refactor ProductItem component to improve type safety

--- src/app/(site)/loja/_components/product-item.tsx
+++ src/app/(site)/loja/_components/product-item.tsx
@@ -1,3 +1,4 @@
+import { Photo, Product } from '@/payload-types';
 import Image from 'next/image';
 import React from 'react';
 
@@ -5,21 +6,30 @@ import PrimaryButton from '@/components/button/primary-button';
 import { H3 } from '@/components/typography';
 
 type ProductItemProps = {
-  product: ProductDTO;
+  product: Product;
 };
 
 export default function ProductItem({ product }: ProductItemProps) {
+  const photos = product.photos as { photo: Photo; description: string }[];
+  const productLink = product.link ?? '';
+  const productPrice = product.price ?? 0;
+
   return (
     <div className={'my-8 text-center'}>
       <div>
-        {product.photos.map(({ photo, description }) => {
+        {photos.map(({ photo, description }) => {
+          const photoUrl = photo.url ?? '';
+          const photoAltText = photo.altText ?? '';
+          const photoWidth = photo.width ?? 0;
+          const photoHeight = photo.height ?? 0;
+
           return (
             <div key={photo.id}>
               <Image
-                src={photo.url}
-                alt={photo.altText ?? ''}
-                width={photo.width}
-                height={photo.height}
+                src={photoUrl}
+                alt={photoAltText}
+                width={photoWidth}
+                height={photoHeight}
               />
               <small>{description}</small>
             </div>
@@ -27,12 +37,12 @@ export default function ProductItem({ product }: ProductItemProps) {
         })}
       </div>
       <H3>{product.name}</H3>
-      <p className={'mb-4'}>R$ {product.price.toFixed(2).replace('.', ',')}</p>
+      <p className={'mb-4'}>R$ {productPrice.toFixed(2).replace('.', ',')}</p>
       <PrimaryButton
         tag={'anchor'}
         target={'_blank'}
         rel={'noreferrer,noopener'}
-        href={product.link}
+        href={productLink}
       >
         Comprar
       </PrimaryButton>
