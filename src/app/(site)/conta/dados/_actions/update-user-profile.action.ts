ba94725e76943906b025d331fe55f5b0e8e33c4f
ghanizadev <jf.melo6@gmail.com>
2025-05-01 21:41:54 +0300

    Enhance user profile validation schema with required fields

--- src/app/(site)/conta/dados/_actions/update-user-profile.action.ts
+++ src/app/(site)/conta/dados/_actions/update-user-profile.action.ts
@@ -6,8 +6,30 @@ import { getPayload } from 'payload';
 import { z } from 'zod';
 
 const updateUserSchema = z.object({
-  name: z.string().optional(),
-  email: z.string().optional(),
+  name: z.string({ required_error: 'Por favor, informe seu nome.' }),
+  email: z
+    .string({ required_error: 'Por favor, informe seu e-mail.' })
+    .email('O formato do e-mail é inválido'),
+  birthday: z
+    .string({ required_error: 'Por favor, informe sua data de aniversário.' })
+    .date('O formato da data de aniversário é inválido'),
+  address: z.string({ required_error: 'Por favor, informe o seu endereço.' }),
+  cpf: z.string({ required_error: 'Por favor, informe seu CPF.' }),
+  rg: z.string({ required_error: 'Por favor, informe seu RG.' }),
+  modality: z
+    .enum(['3km (caminhada)', '5km', '10km', '21km', '42km'], {
+      required_error: 'Por favor, informe a modalidade da sua corrida.',
+      message: 'Por favor, informe a modalidade da sua corrida.',
+    })
+    .optional(),
+  'tshirt.type': z.enum(['masc', 'fem', 'inf'], {
+    required_error: 'Por favor, informe o tipo da sua camiseta.',
+    message: 'Por favor, informe o tipo da sua camiseta.',
+  }),
+  'tshirt.size': z.enum(['P', 'M', 'G', 'XG'], {
+    required_error: 'Por favor, informe o tamanho da sua camiseta.',
+    message: 'Por favor, informe o tamanho da sua camiseta.',
+  }),
 });
 
 export default async function updateUserProfileAction(
