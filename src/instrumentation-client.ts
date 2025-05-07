332519e1878ef82ab939fec7dbeaff2ded29849e
kadu0510 <karloseflorindo@gmail.com>
2025-05-07 17:03:21 -0300

    Add sentry to the code

---
+++ src/instrumentation-client.ts
@@ -0,0 +1,17 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://45a8c535fdf97ba315e527ae5e2110d8@o4509283585359873.ingest.de.sentry.io/4509283595845712",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
